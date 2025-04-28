eed7f7426ddf306c716db17e165bf7ed5d4279ee
Brendan Poma <89bpoma@gmail.com>
2025-04-27 23:10:13 -0700

    Hour 10
    worked on an completed Hour 10 to the best of my abilities.

---
+++ bpoma_hour10/Assets/GameCritic.cs
@@ -0,0 +1,21 @@
+/*
+1. I wouildn't really consider the game hard or easy it is kind of up to chance a lot of 
+the time because of the way that the Chaos ball move and interact with the colored balls as well as the arena. 
+If I had to pick one or the other I would probably call the game hard
+
+2. I think there are a lot of ways you can make the game easier or harder and example of a way to make it harder would be to 
+add more chaos ball or to make the arena itself bigger that before. If you wanted to make it easier you could decrease the overall speed of the 
+object moving about or you could make the bumper bigger overall.
+
+3. Something that would give the game a 'wow' factor would be to write some sort of script that 
+makes the goals flash all the other goals colors when the goal is scored on, but that seems really complicated. 
+Something else you could do would be to just give the game a reset button so that playing is always fast and easy to restart
+
+4. I think overall the ball bouncing around the arena and into the bumper is really fun and while it can be difficult, trying to predict and hit the balls can be fun and challening. 
+This can also be tedious at times when the Chaos balls interfere a ton with the colored balls.
+
+ - Changed the size of the Bumper.
+ - Added Variations to the Chaos Balls size.
+ - Added other Game objects to the borders to create more Chaos!
+ - was going to change the sound of the footsteps but was having trouble importing the asset unfortunately and ran out of time because I started working too late...
+ */
