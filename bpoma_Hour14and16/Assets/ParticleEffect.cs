ee3c44273b4efd919140af12bc42b34568e4bc51
Brendan Poma <89bpoma@gmail.com>
2025-05-11 13:43:32 -0700

    Hour 14 and 16
    completed all TIY for both hours as well as the specified exercises for each hour.

---
+++ bpoma_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+ The 5 modifications i made to the parameters were:
+- changed the shape of the particle system to square.
+- changed color over time so that as the particles spawned by the end of their course they would be a different color.
+- changed the starting size of the particles so that they would vary between different sizes at the start.
+- used the curves editor to create more variation in size as the particles travel.
+- changed the velocity over time to create an interesting route for the particles to travel on.
+ */
---
+++ bpoma_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,8 @@
+/*
+ The modifications I made were:
+- changed background image to that of a sapce theme.
+- changed the color of the text on the button as well as its location, size, and color.
+- created a title with unique sizing and font.
+- added additional text with varying font style that give a theme to the game.
+- used the background image to place text and button in interesting places that left the background image more open.
+*/
