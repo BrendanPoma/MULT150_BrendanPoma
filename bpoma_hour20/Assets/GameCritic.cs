dc3b64f4aa83907d4363c56d4457e2cda1152a2a
Brendan Poma <89bpoma@gmail.com>
2025-05-21 17:24:48 -0700

    Finished Assignment
    I could not figure out how to get my new Obstacle prefab to spawn and it was frustrating.

---
+++ bpoma_hour20/Assets/GameCritic.cs
@@ -0,0 +1,21 @@
+/*
+1. I would definitely say mechanically the game is simple but its HARD. I say this because the Player is only on one set path and if you 'Phase Out' to not get hit by one of the obstacles often by 
+the time you 'Phase In' an obstacle may already be on you again with no way to avoid it. you could potentially get around it by just spamming the 'Phase Out' mechanic but that is kind of lame in my opinion.
+So my two reasons for why I think it's too had is because you are on a set path and the spawn rate on obstacles may be too fast for being in a fixed position.
+
+2.A change that I would make to make the game easier would be to either slow down the overall speed of the objects coming at the player or make it so that the player can move side to side as another way to avoid 
+the obstacles. To make the game harder id either add vairety to the obstacles coming at the player or increase the speed at which the obstacles come at the player.
+
+3.One thing that would give the game somewhat of a 'wow' factor could be to create some sort of animation or effect when the player collects a Power Up, it might even add to the game if you create a counter for 
+how many Power Up's the player has gathered total in one run. Another thing that would make the game for special is if there was some verticality added. Maybe change some obstacles so that you have to jump over 
+some while using the 'Phase Out' mechanic to avoid other obstacles.
+ 
+4. I think that the Phase mechanic itself is really fun especially seeing the model go transluscent. While it is challenging it also fun to really see how long you can stay in the game for also.
+
+5. I think definitely at times having to spam the Phase Out mechanic just to survive at times can be annoying and as I've mentioned a couple times throughout my responses I think that not being able to move horizontally 
+to avoid obstacles or grab power ups is not really tedious is definitely annoying.
+
+ Obstacle Prefab Changes: I created a whole new obstacle prefab out of a cylinder and changed the color and size of it. However for some reason I cant get the Game Manger to spawn them i must be just missing something unfortunately.
+ 
+ Power-Up Prefab Changes: particle color, size over time, change shape to hemisphere, and start size.
+ */
