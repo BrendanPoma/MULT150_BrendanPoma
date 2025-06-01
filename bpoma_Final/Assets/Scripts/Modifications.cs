2588e7ecaee7f085e97ba58385dc414135fa4f41
Brendan Poma <89bpoma@gmail.com>
2025-05-31 22:48:15 -0700

    Chaos Ball Rework
    Finished Reworking the Chaos Ball game, unfortunately I got sick this weekend when i planned on working on the assignment the most so unfortunately the UI and the Animation wont get done. I had a great time this semester and thanks for teaching me so much about unity. I hope you have another great semester next year
    
    -Brendan Poma

---
+++ bpoma_Final/Assets/Scripts/Modifications.cs
@@ -0,0 +1,12 @@
+/*
+1. Directional light, changed: color pink, intensity 0.75.
+2. Added Fog to the scene.
+3. Skybox, changed: sun size, convergence, atmosphere thickness, sky + ground tint, and exposure. 
+4. Created Chaos ball prefab.
+5. Added particle system: changed shape, emission, and rotation over time
+6. Made a prefab for each colored ball.
+7. Added a unique texture to each colored ball.
+8. Changed colors to different shade of colored balls for the goals also changed the values for intensity and shadows to soft.
+9. Created unique walls with columns at the corners as well as two with varying heights.
+10. Tried to keep the hierarchy clean as well as added textures to the walls as well as the ground and columns.
+*/
